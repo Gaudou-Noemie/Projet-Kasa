5a06dc0b801b93b722d98b1a27a9392f8357f0db
Gaudou Noémie <gaudou.noemie@gmail.com>
2024-11-20 19:29:30 +0100

    mise en place des collapses

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import "./collapse.scss";
 
-function Collapse({ title, children }) {
+function Collapse({ title, children, id }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleCollapse = () => {
@@ -11,7 +11,7 @@ function Collapse({ title, children }) {
   };
 
   return (
-    <div className="collapse">
+    <div id={id} className="collapse">
       <div className="collapse__header" onClick={toggleCollapse}>
         <h3 className="collapse__title">{title}</h3>
         <span
--- src/components/FullCard/index.jsx
+++ src/components/FullCard/index.jsx
@@ -1,14 +1,36 @@
 // Création de la fonction qui affiche les informations du logement
-
+import Collapse from "../Collapse";
 import { useParams } from "react-router-dom";
 import Carousel from "../Carousel";
 import datas from "../../Data/Data";
+import "./fullcard.scss";
 
 function FullCard() {
   const { id } = useParams();
   const logement = datas.find((data) => data.id === id);
+  const titre = datas.find((title) => title.id === id);
+  const localisation = datas.find((location) => location.id === id);
 
-  return <Carousel pictures={logement.pictures} />;
+  return (
+    <div>
+      <Carousel pictures={logement.pictures} />
+      <h2>{titre.title}</h2>
+      <h3>{localisation.location}</h3>
+
+      <div className="collapse">
+        <Collapse className="collapse__desc" title="Description">
+          <p>{logement.description}</p>
+        </Collapse>
+        <Collapse className="collapse__equi" title="Equipements">
+          <ul>
+            {logement.equipments.map((equipments, index) => (
+              <li key={index}>{equipments}</li>
+            ))}
+          </ul>
+        </Collapse>
+      </div>
+    </div>
+  );
 }
 
 export default FullCard;
